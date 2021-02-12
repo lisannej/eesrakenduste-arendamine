9796e238f33d897d1ef3c884c1d54df940af10f9
Lisanne Järv <lisannejarv@gmail.com>
2021-02-12 08:39:44 +0200

    ..

---
+++ first.js
@@ -0,0 +1,4 @@
+function demoFunction(){
+   let demoElement= document.getElementById('demo');
+   demoElement.innerHTML= "Tegin sisus muudatuse nupu abil";
+}
---
+++ second.js
@@ -0,0 +1,13 @@
+const first = document.getElementById('first');
+
+let massiiv = ['esimene', 'teine', 'kolmas'];
+
+//window.alert(massiiv[0]);
+
+for(let i =0; i< massiiv.length, i++){
+    console.log(massiiv[i]); 
+}
+
+massiiv.forEach(function(){
+    window.alert();
+});
