45aae92ec3bdf4c0848488c994fe66adb69aaef5
Lisanne Järv <lisannejarv@gmail.com>
2021-02-12 08:53:58 +0200

    teine tund kaib

--- second.js
+++ second.js
@@ -1,6 +1,12 @@
 const first = document.getElementById('first');
 
 let massiiv = ['esimene', 'teine', 'kolmas'];
+let numbers = [1, 2, 3, 4, 5, 6];
+
+numbers.sort(function(a,b) {return a-b});
+
+massiiv.sort();
+massiiv.reverse();
 
 //window.alert(massiiv[0]);
 
@@ -8,6 +14,8 @@ for(let i =0; i< massiiv.length, i++){
     console.log(massiiv[i]); 
 }
 
-massiiv.forEach(function(){
-    window.alert();
+massiiv.forEach(function(item){
+    console.log(item);
+    let text = '<li>'+ item + '</li>';
+    first.innerHTML = first.innerHTML + text;
 });
