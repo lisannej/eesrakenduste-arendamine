8af49671f3ee304fbcfab71b936f84239c8c035a
Lisanne Järv <lisannejarv@gmail.com>
2021-03-12 13:55:34 +0200

    Update clock.js

--- esimene-kodutoo/clock.js
+++ esimene-kodutoo/clock.js
@@ -17,7 +17,6 @@ function updateClock(){
     var date = curWeekDay+", "+curDay+" "+curMonth+" "+curYear;
     document.getElementById("date").innerHTML = date;
     
-    var time = setTimeout(function(){ updateClock() }, 500);
 }
 
 function checkTime(i) {
