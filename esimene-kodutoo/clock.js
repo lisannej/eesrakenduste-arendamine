88f1310b2ad76e5b0d83d18fd4a42dd91aaf7812
Lisanne Järv <lisannejarv@gmail.com>
2021-03-12 17:09:07 +0200

    k

--- esimene-kodutoo/clock.js
+++ esimene-kodutoo/clock.js
@@ -39,4 +39,11 @@ function checkTime(i) {
 
   updateClock();
   
-  
+  function ChangeColor(){  
+    if (document.getElementById('bgbutton').style.backgroundColor == 'Black'){
+      document.getElementById('bgbutton').style.backgroundColor = 'White';
+    } else {
+      document.getElementById('bgbutton').style.backgroundColor = 'Black';
+
+    }
+  }
