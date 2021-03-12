3727148ef84e600862b0334e4b12fb5269434104
Lisanne Järv <lisannejarv@gmail.com>
2021-03-12 15:52:09 +0200

    Update clock.js

--- esimene-kodutoo/clock.js
+++ esimene-kodutoo/clock.js
@@ -39,19 +39,14 @@ function checkTime(i) {
 
   updateClock();
 
-const bgbutton = document.querySelector('bgbutton')
-const body = document.querySelector('body')
-const colors = ['black', 'white']
-
-body.style.backgroundColor = 'black'
-button.addEventListener('click', changeBackground)
-
-function changeBackground(){
-  if (body == 'black') {
-    document.body.style.backgroundColor = 'white';
-    document.body.style.Color = 'white';
-} else {
-    document.body.style.backgroundColor = 'black';
-}
-body.style.backgroundColor = colors[colorIndex]
-}
+  const button = document.querySelector('bgbutton')
+  const body = document.querySelector('body')
+  const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+  
+  body.style.backgroundColor = 'violet'
+  button.addEventListener('click', changeBackground)
+  
+  function changeBackground(){
+  const colorIndex= parseInt(Math.random()*colors.length)
+  body.style.backgroundColor = colors[colorIndex]
+  }
