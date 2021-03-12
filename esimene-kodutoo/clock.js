fc439d4adac391621283210fd1228d4056203e70
Lisanne Järv <lisannejarv@gmail.com>
2021-03-12 14:23:09 +0200

    Update clock.js

--- esimene-kodutoo/clock.js
+++ esimene-kodutoo/clock.js
@@ -8,31 +8,30 @@ function greeting(){
     greetings = "Good Evening! ";
   }
   document.getElementById("greeting").innerHTML = greetings ;
+
+
+  function updateClock(){
+    var today = new Date();
+    var hr = today.getHours(); 
+    var min = today.getMinutes(); 
+    var sec = today.getSeconds(); 
+
+    min = checkTime(min);
+    sec = checkTime(sec);
+    document.getElementById("Clock").innerHTML = hr + " : " + min + " : " + sec;
+
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      var curWeekDay = days[today.getDay()];
+      var curDay = today.getDate();
+      var curMonth = months[today.getMonth()];
+      var curYear = today.getFullYear();
+      var date = curWeekDay+", "+curDay+" "+curMonth+" "+curYear;
+      document.getElementById("date").innerHTML = date;
+      
+      var time = setTimeout(function(){ updateClock() }, 500);
+  }
 }
-
-
-function updateClock(){
-  var today = new Date();
-  var hr = today.getHours(); 
-  var min = today.getMinutes(); 
-  var sec = today.getSeconds(); 
-
-  min = checkTime(min);
-  sec = checkTime(sec);
-  document.getElementById("Clock").innerHTML = hr + " : " + min + " : " + sec;
-
-  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var curWeekDay = days[today.getDay()];
-    var curDay = today.getDate();
-    var curMonth = months[today.getMonth()];
-    var curYear = today.getFullYear();
-    var date = curWeekDay+", "+curDay+" "+curMonth+" "+curYear;
-    document.getElementById("date").innerHTML = date;
-    
-    var time = setTimeout(function(){ updateClock() }, 500);
-}
-
 
 function checkTime(i) {
   if (i < 10) {
@@ -41,12 +40,5 @@ function checkTime(i) {
   return i;
 }
 
-function updateTextSize() {
-  for (var i = 0; 3 > i; i++) {  // Iterate for better better convergence
-    curFontSize *= targetWidth / (textElem.offsetWidth / textElem.parentNode.offsetWidth);
-    textElem.style.fontSize = curFontSize + "pt";
-  }
-}
   updateClock();
 	updateTextSize();
-	window.addEventListener("resize", updateTextSize);
