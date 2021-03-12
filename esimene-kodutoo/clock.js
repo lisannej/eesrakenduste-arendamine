c7fdb16880dae6fa102c858a3e1a5998cb48998a
Lisanne Järv <lisannejarv@gmail.com>
2021-03-12 14:27:28 +0200

    Update clock.js

--- esimene-kodutoo/clock.js
+++ esimene-kodutoo/clock.js
@@ -1,3 +1,27 @@
+
+
+function updateClock(){
+  var today = new Date();
+  var hr = today.getHours(); 
+  var min = today.getMinutes(); 
+  var sec = today.getSeconds(); 
+
+  min = checkTime(min);
+  sec = checkTime(sec);
+  document.getElementById("Clock").innerHTML = hr + " : " + min + " : " + sec;
+
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var curWeekDay = days[today.getDay()];
+    var curDay = today.getDate();
+    var curMonth = months[today.getMonth()];
+    var curYear = today.getFullYear();
+    var date = curWeekDay+", "+curDay+" "+curMonth+" "+curYear;
+    document.getElementById("date").innerHTML = date;
+    
+    var time = setTimeout(function(){ updateClock() }, 500);
+}
+
 function greeting(){
   //var greetings = "";
   if (hr < 12){
@@ -8,30 +32,8 @@ function greeting(){
     greetings = "Good Evening! ";
   }
   document.getElementById("greeting").innerHTML = greetings ;
-
-
-  function updateClock(){
-    var today = new Date();
-    var hr = today.getHours(); 
-    var min = today.getMinutes(); 
-    var sec = today.getSeconds(); 
-
-    min = checkTime(min);
-    sec = checkTime(sec);
-    document.getElementById("Clock").innerHTML = hr + " : " + min + " : " + sec;
-
-    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-      var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-      var curWeekDay = days[today.getDay()];
-      var curDay = today.getDate();
-      var curMonth = months[today.getMonth()];
-      var curYear = today.getFullYear();
-      var date = curWeekDay+", "+curDay+" "+curMonth+" "+curYear;
-      document.getElementById("date").innerHTML = date;
-      
-      var time = setTimeout(function(){ updateClock() }, 500);
-  }
 }
+
 
 function checkTime(i) {
   if (i < 10) {
